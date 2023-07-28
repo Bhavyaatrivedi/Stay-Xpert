a66bd2ba5b5602207208776d1e6c0d5291b34a4b
Bhavyaa Trivedi <bhavyaatrivedi2002@gmail.com>
2023-07-28 12:11:36 +0530

    User Schema

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express();
 
 app.use(express.json());
@@ -8,6 +10,9 @@ app.use(cors({
     credentials: true,
     origin: 'http://127.0.0.1:5173'
 }));
+
+console.log(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL);
 
 app.get('/test', (req, res) =>{
     res.json('test ok')
