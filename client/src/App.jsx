17bd57624dfedf3e4fdc8899826ad27126d61951
Bhavyaa Trivedi <bhavyaatrivedi2002@gmail.com>
2023-07-28 11:37:16 +0530

    mongoose connection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
 import {Route, Routes} from "react-router-dom";
 import IndexPage from "./page/IndexPage";
 import LoginPage from "./page/LoginPage";
+import RegisterPage from "./page/RegisterPage"
 import Layout from "./Layout";
+import axios from "axios";
+
+axios.defaults.baseURL = 'http://localhost:4000'
 
 function App() {
  
@@ -10,6 +14,7 @@ function App() {
     <Route path="/" element={<Layout/>}/>
     <Route index element = {<IndexPage />}/>
     <Route path="/login" element={<LoginPage/>}/>
+    <Route path="/register" element={<RegisterPage/>}/>
    </Routes>
   )
 }
